9cf9546f2c730ccbf2c098cb82c89f60935eefeb
Jilson Martins Junior <jilsonmartins.jr@gmail.com>
2021-08-10 17:42:51 -0300

    Update

--- IMC/assets/js/main.js
+++ IMC/assets/js/main.js
@@ -8,18 +8,44 @@ form.addEventListener('submit', function (event) {
     const peso = Number(inputPeso.value);
     const altura = Number(inputAltura.value);
 
-    console.log(peso,altura)
+    if (!peso) {
+        setResultado('Peso inválido!', false);
+        return;
+    }
 
+    if (!altura) {
+        setResultado('Altura inválido!', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+
+    console.log("Cheguei aqui")
 });
+
+function getNivelImc() {
+      const nivel = ['Abaixo do peso', 'peso normal', 'Sobrepeso', 'Obsidade grau 1', 'Obsidade grau 2', 'Obsidade grau 3'];
+
+      if (imc >= 39.9) {
+          return nivel[]
+      } else if ()
+}
+
+function getImc(peso, altura) {
+     const imc = peso / altura ** 2;
+     return imc.toFixed(2);
+}
 
 function criaParagrafo () {
     const p = document.createElement('p');
     return p;
 }
 
-function setResultado (msg) {
+function setResultado (msg, isValid) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
 
     const p =  criaParagrafo();
+    p.innerHTML = msg;
+    resultado.appendChild(p)
 }
